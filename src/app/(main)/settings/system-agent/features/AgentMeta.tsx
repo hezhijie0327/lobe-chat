f138b3dd0f2d9d885e8ff0227de2126dd25d747a
Zhijie He <hezhijie0327@hotmail.com>
2024-06-30 16:57:38 +0800

    Update AgentMeta.tsx

--- src/app/(main)/settings/system-agent/features/AgentMeta.tsx
+++ src/app/(main)/settings/system-agent/features/AgentMeta.tsx
@@ -22,7 +22,7 @@ const AgentMeta = memo(() => {
   const settings = useUserStore(settingsSelectors.currentSystemAgent, isEqual);
   const [updateSystemAgent] = useUserStore((s) => [s.updateSystemAgent]);
 
-  const [form] = AForm.useForm();
+  const [form] = AntForm.useForm();
   useEffect(() => {
     form.setFieldsValue(settings);
   }, [settings, form]);
