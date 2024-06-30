cca7399b7b326e675e87fe5aaa34400296dcb7be
Zhijie He <hezhijie0327@hotmail.com>
2024-06-30 20:04:40 +0800

    Update baichuan.ts

--- src/config/modelProviders/baichuan.ts
+++ src/config/modelProviders/baichuan.ts
@@ -4,22 +4,29 @@ import { ModelProviderCard } from '@/types/llm';
 const Baichuan: ModelProviderCard = {
   chatModels: [
     {
+      displayName: 'Baichuan 4',
+      functionCall: true,
       id: 'Baichuan4',
       tokens: 32_768,
     },
     {
+      displayName: 'Baichuan 3 Turbo',
+      functionCall: true,
       id: 'Baichuan3-Turbo',
       tokens: 32_768,
     },
     {
+      displayName: 'Baichuan 3 Turbo 128k',
       id: 'Baichuan3-Turbo-128k',
       tokens: 128_000,
     },
     {
+      displayName: 'Baichuan 2 Turbo',
       id: 'Baichuan2-Turbo',
       tokens: 32_768,
     },
     {
+      displayName: 'Baichuan 2 Turbo 192k',
       id: 'Baichuan2-Turbo-192k',
       tokens: 192_000,
     },
