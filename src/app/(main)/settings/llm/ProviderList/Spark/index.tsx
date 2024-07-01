ba8457ba78f54a6539d058f866999fc7dfbd4595
Zhijie He <hezhijie0327@hotmail.com>
2024-07-01 12:17:57 +0800

    Create index.tsx

---
+++ src/app/(main)/settings/llm/ProviderList/Spark/index.tsx
@@ -0,0 +1,46 @@
+'use client';
+
+import { Input } from 'antd';
+import { useTranslation } from 'react-i18next';
+
+import { SparkProviderCard } from '@/config/modelProviders';
+import { GlobalLLMProviderKey } from '@/types/user/settings';
+
+import { KeyVaultsConfigKey } from '../../const';
+import { ProviderItem } from '../../type';
+import { SparkBrand } from '../providers';
+
+const providerKey: GlobalLLMProviderKey = 'spark';
+
+export const useSparkProvider = (): ProviderItem => {
+  const { t } = useTranslation('modelProvider');
+
+  return {
+    ...useSparkProviderCard,
+    apiKeyItems: [
+      {
+        children: (
+          <Input.Password
+            autoComplete={'new-password'}
+            placeholder={t(`${providerKey}.sparkApiKey.placeholder`)}
+          />
+        ),
+        desc: t(`${providerKey}.sparkApiKey.desc`),
+        label: t(`${providerKey}.sparkApiKey.title`),
+        name: [KeyVaultsConfigKey, providerKey, 'sparkApiKey'],
+      },
+      {
+        children: (
+          <Input.Password
+            autoComplete={'new-password'}
+            placeholder={t(`${providerKey}.sparkApiSecret.placeholder`)}
+          />
+        ),
+        desc: t(`${providerKey}.sparkApiSecret.desc`),
+        label: t(`${providerKey}.sparkApiSecret.title`),
+        name: [KeyVaultsConfigKey, providerKey, 'sparkApiSecret'],
+      },
+    ],
+    title: <SparkBrand />,
+  };
+};
