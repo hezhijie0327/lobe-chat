9c42e49d9f6792284d60e6223f7247a3492bf45c
Zhijie He <hezhijie0327@hotmail.com>
2024-07-01 13:24:41 +0800

    Update providers.tsx

--- src/app/(main)/settings/llm/ProviderList/providers.tsx
+++ src/app/(main)/settings/llm/ProviderList/providers.tsx
@@ -185,7 +185,7 @@ export const useProviderList = (): ProviderItem[] => {
       {
         ...SparkProviderCard,
         docUrl: urlJoin(BASE_DOC_URL, 'spark'),
-        title: <Spark.Combine size={ 20 } type={ 'color' } />,
+        title: <SparkBrand />,
       },
     ],
     [azureProvider, ollamaProvider, ollamaProvider, bedrockProvider, sparkProvider],
