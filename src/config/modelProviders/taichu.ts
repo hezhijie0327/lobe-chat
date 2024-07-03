9760552260194e61f268d3d3e0eaad1e183fd640
Zhijie He <hezhijie0327@hotmail.com>
2024-07-03 08:38:36 +0800

    Create taichu.ts

---
+++ src/config/modelProviders/taichu.ts
@@ -0,0 +1,30 @@
+import { ModelProviderCard } from '@/types/llm';
+
+// ref https://ai-maas.wair.ac.cn/#/doc
+const Taichu: ModelProviderCard = {
+  chatModels: [
+    {
+      description: '语言大模型',
+      displayName: 'Taichu-2.0',
+      enabled: true,
+      functionCall: true,
+      id: 'taichu_llm',
+      tokens: 32_768,
+    },
+    {
+      description: '多模态大模型',
+      displayName: 'Taichu-2.0V',
+      enabled: true,
+      functionCall: true,
+      id: 'taichu_vqa',
+      tokens: 4096,
+      vision: true,
+    },
+  ],
+  checkModel: 'taichu_llm',
+  id: 'taichu',
+  modelList: { showModelFetcher: true },
+  name: 'Taichu',
+};
+
+export default Taichu;
