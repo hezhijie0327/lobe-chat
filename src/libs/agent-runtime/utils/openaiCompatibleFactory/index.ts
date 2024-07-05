41427dfe72870f1cf1a3db60d4437037293031ee
Zhijie He <hezhijie0327@hotmail.com>
2024-07-05 16:16:39 +0800

    Update index.ts

--- src/libs/agent-runtime/utils/openaiCompatibleFactory/index.ts
+++ src/libs/agent-runtime/utils/openaiCompatibleFactory/index.ts
@@ -86,10 +86,8 @@ export const LobeOpenAICompatibleFactory = ({
             } as OpenAI.ChatCompletionCreateParamsStreaming);
 
         const response = await this.client.chat.completions.create(
-          {
-            ...postPayload,
-            ...(debug?.includeUser ? { user: options?.user } : {}),
-          },
+          ...postPayload,
+          ...(debug?.includeUser ? { user: options?.user } : {}),
           {
             // https://github.com/lobehub/lobe-chat/pull/318
             headers: { Accept: '*/*' },
