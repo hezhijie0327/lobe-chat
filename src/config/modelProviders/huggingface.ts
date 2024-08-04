d289d3a9a472a30ba60ba157db597050a9f8d39d
Zhijie He <hezhijie0327@hotmail.com>
2024-08-04 11:22:07 +0800

    Create huggingface.ts

---
+++ src/config/modelProviders/huggingface.ts
@@ -0,0 +1,20 @@
+import { ModelProviderCard } from '@/types/llm';
+
+// ref https://huggingface.co/models?pipeline_tag=text-generation&sort=trending
+const HuggingFaceAI: ModelProviderCard = {
+  chatModels: [
+    {
+      displayName: 'Llama 3.1 8B Instruct',
+      enabled: true,
+      functionCall: false,
+      id: 'meta-llama/Meta-Llama-3.1-8B-Instruct',
+      tokens: 131_072,
+    },
+  ],
+  checkModel: 'meta-llama/Meta-Llama-3.1-8B-Instruct',
+  id: 'huggingface',
+  modelList: { showModelFetcher: true },
+  name: 'Hugging Face',
+};
+
+export default HuggingFaceAI;
