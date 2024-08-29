344b83f50641068397a560f3a4da236a4a82f8e1
Zhijie He <hezhijie0327@hotmail.com>
2024-08-29 22:01:02 +0800

    Update agentRuntime.ts

--- src/app/api/chat/agentRuntime.ts
+++ src/app/api/chat/agentRuntime.ts
@@ -200,19 +200,19 @@ const getLlmOptionsFromPayload = (provider: string, payload: JWTPayload) => {
 
       return { apiKey };
     }
-    case ModelProvider.Upstage: {
-      const { UPSTAGE_API_KEY, UPSTAGE_PROXY_URL } = getLLMConfig();
-
-      const apiKey = apiKeyManager.pick(payload?.apiKey || UPSTAGE_API_KEY);
-      const baseURL = payload?.endpoint || UPSTAGE_PROXY_URL;
-
-      return { apiKey, baseURL };
-    }
     case ModelProvider.SiliconCloud: {
       const { SILICONCLOUD_API_KEY, SILICONCLOUD_PROXY_URL } = getLLMConfig();
 
       const apiKey = apiKeyManager.pick(payload?.apiKey || SILICONCLOUD_API_KEY);
       const baseURL = payload?.endpoint || SILICONCLOUD_PROXY_URL;
+
+      return { apiKey, baseURL };
+    }
+    case ModelProvider.Upstage: {
+      const { UPSTAGE_API_KEY, UPSTAGE_PROXY_URL } = getLLMConfig();
+
+      const apiKey = apiKeyManager.pick(payload?.apiKey || UPSTAGE_API_KEY);
+      const baseURL = payload?.endpoint || UPSTAGE_PROXY_URL;
 
       return { apiKey, baseURL };
     }
