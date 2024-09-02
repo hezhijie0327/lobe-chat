5bf6230e99c2721ca561be96596f9aecddecee88
Zhijie He <hezhijie0327@hotmail.com>
2024-09-02 11:44:44 +0800

    Create index.ts

---
+++ src/libs/agent-runtime/ai21/index.ts
@@ -0,0 +1,10 @@
+import { ModelProvider } from '../types';
+import { LobeOpenAICompatibleFactory } from '../utils/openaiCompatibleFactory';
+
+export const LobeAi21AI = LobeOpenAICompatibleFactory({
+  baseURL: 'https://api.ai21.com/studio/v1',
+  debug: {
+    chatCompletion: () => process.env.DEBUG_AI21_CHAT_COMPLETION === '1',
+  },
+  provider: ModelProvider.Ai21,
+});
