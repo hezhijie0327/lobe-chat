399ffa742f670f7d892cfef5739ba42e579a3610
Zhijie He <hezhijie0327@hotmail.com>
2024-09-07 19:07:12 +0800

    Update ai21.ts

--- src/libs/agent-runtime/utils/streams/bedrock/ai21.ts
+++ src/libs/agent-runtime/utils/streams/bedrock/ai21.ts
@@ -19,6 +19,7 @@ interface AmazonBedrockInvocationMetrics {
 }
 
 // https://docs.aws.amazon.com/bedrock/latest/userguide/model-parameters-jamba.html
+// ai21_chunk: {"id":"chat-ae86a1e555f04e5cbddb86cc6a98ce5e","choices":[{"index":0,"delta":{"content":"?"},"finish_reason":"stop","stop_reason":"<|eom|>"}],"usage":{"prompt_tokens":144,"total_tokens":158,"completion_tokens":14},"meta":{"requestDurationMillis":146}},"amazon-bedrock-invocationMetrics":{"inputTokenCount":63,"outputTokenCount":263,"invocationLatency":5330,"firstByteLatency":122}}
 interface BedrockAi21StreamChunk {
   'amazon-bedrock-invocationMetrics'?: AmazonBedrockInvocationMetrics;
   'choices': {
@@ -45,15 +46,13 @@ export const transformAi21Stream = (
   stack: StreamStack,
 ): StreamProtocolChunk => {
   // remove 'amazon-bedrock-invocationMetrics' from chunk
-  // ai21_chunk: "amazon-bedrock-invocationMetrics":{"inputTokenCount":63,"outputTokenCount":263,"invocationLatency":5330,"firstByteLatency":122}}
   delete chunk['amazon-bedrock-invocationMetrics'];
 
   if (!chunk.choices || chunk.choices.length === 0) {
     return { data: chunk, id: stack.id, type: 'data' };
   }
 
-  // ai21_chunk: {"id":"chat-ae86a1e555f04e5cbddb86cc6a98ce5e","choices":[{"index":0,"delta":{"content":"?"},"finish_reason":"stop","stop_reason":"<|eom|>"}],"usage":{"prompt_tokens":144,"total_tokens":158,"completion_tokens":14},"meta":{"requestDurationMillis":146}}
-  const item = chunk.choices[0];
+ const item = chunk.choices[0];
   if (!item) {
     return { data: chunk, id: stack.id, type: 'data' };
   }
