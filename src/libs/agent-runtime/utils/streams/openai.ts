65a1bb11a94985badac5cc2b47dad5a218c865f0
Zhijie He <hezhijie0327@hotmail.com>
2024-09-16 05:43:15 +0800

    Update openai.ts

--- src/libs/agent-runtime/utils/streams/openai.ts
+++ src/libs/agent-runtime/utils/streams/openai.ts
@@ -62,11 +62,9 @@ export const transformOpenAIStream = (
       // one-api 的流式接口，会出现既有 finish_reason ，也有 content 的情况
       //  {"id":"demo","model":"deepl-en","choices":[{"index":0,"delta":{"role":"assistant","content":"Introduce yourself."},"finish_reason":"stop"}]}
 
-      /*
       if (typeof item.delta?.content === 'string' && !!item.delta.content) {
         return { data: item.delta.content, id: chunk.id, type: 'text' };
       }
-      */
 
       return { data: item.finish_reason, id: chunk.id, type: 'stop' };
     }
