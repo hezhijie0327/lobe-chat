9e3724bb6852dd6df9d2850e520ae2c727d23b17
Zhijie He <hezhijie0327@hotmail.com>
2024-09-27 14:55:10 +0800

    Update jwt.ts

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -37,7 +37,7 @@ export const createJWT = async <T>(payload: T) => {
 };
 
 // https://console.sensecore.cn/help/docs/model-as-a-service/nova/overview/Authorization
-export const encodeJwtTokenSenseCore = async (ak: any, sk: any): Promise<string> => {
+export const encodeJwtTokenSenseCore = async (ak: any, sk: any): Promise<{ apiKey: string }> => {
     const secret = new TextEncoder().encode(sk);
     const apiKey = await new SignJWT({
             iss: ak,
