d7437c9dd0a48ec2fe77182ca738dd20e3e5445e
Zhijie He <hezhijie0327@hotmail.com>
2024-09-29 10:48:03 +0800

    Create authToken.ts

---
+++ src/libs/agent-runtime/sensenova/authToken.ts
@@ -0,0 +1,34 @@
+import CryptoJS from 'crypto-js';
+
+const base64UrlEncode = (obj: object) => {
+    return CryptoJS.enc.Base64.stringify(CryptoJS.enc.Utf8.parse(JSON.stringify(obj)))
+      .replace(/=/g, '')
+      .replace(/\+/g, '-')
+      .replace(/\//g, '_')
+}
+
+// https://console.sensecore.cn/help/docs/model-as-a-service/nova/overview/Authorization
+export const caculateJwtToken = (accessKeyID?: string, accessKeySecret?: string) => {
+      const headers = {
+        alg: 'HS256',
+        typ: 'JWT'
+      }
+
+      const payload = {
+        exp: Math.floor(Date.now() / 1000) + 1800, // Current Time + 30m
+        iss: accessKeyID,
+        nbf: Math.floor(Date.now() / 1000) - 5     // Current Time - 5s
+      }
+
+      const data = `${ base64UrlEncode(headers) }.${ base64UrlEncode(payload) }`
+
+      const signature = CryptoJS.HmacSHA256(data, accessKeySecret)
+        .toString(CryptoJS.enc.Base64)
+        .replace(/=/g, '')
+        .replace(/\+/g, '-')
+        .replace(/\//g, '_')
+
+      const apiKey = `${ data }.${ signature }`
+
+      return apiKey
+};
