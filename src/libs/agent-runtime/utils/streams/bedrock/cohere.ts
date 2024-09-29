cead8a4dddf0e345ee600f71ddb6f77e3bfd1051
Zhijie He <hezhijie0327@hotmail.com>
2024-09-29 22:52:29 +0800

    Create cohere.ts

---
+++ src/libs/agent-runtime/utils/streams/bedrock/cohere.ts
@@ -0,0 +1,56 @@
+import { InvokeModelWithResponseStreamResponse } from '@aws-sdk/client-bedrock-runtime';
+
+import { nanoid } from '@/utils/uuid';
+
+import { ChatStreamCallbacks } from '../../../types';
+import {
+  StreamProtocolChunk,
+  StreamProtocolToolCallChunk,
+  StreamStack,
+  StreamToolCallChunkData,
+  createCallbacksTransformer,
+  createSSEProtocolTransformer,
+  generateToolCallId,
+} from '../protocol';
+import { createBedrockStream } from './common';
+
+interface AmazonBedrockInvocationMetrics {
+  firstByteLatency: number;
+  inputTokenCount: number;
+  invocationLatency: number;
+  outputTokenCount: number;
+}
+
+// https://docs.aws.amazon.com/bedrock/latest/userguide/model-parameters-cohere-command-r-plus.html
+interface BedrockCohereStreamChunk {
+  'amazon-bedrock-invocationMetrics'?: AmazonBedrockInvocationMetrics;
+  'finish_reason': string;
+  'text: string;
+}
+
+export const transformCohereStream = (
+  chunk: BedrockCohereStreamChunk,
+  stack: StreamStack,
+): StreamProtocolChunk => {
+  // remove 'amazon-bedrock-invocationMetrics' from chunk
+  delete chunk['amazon-bedrock-invocationMetrics'];
+
+  if (chunk.finish_reason) {
+    return { data: chunk.finish_reason, id: stack.id, type: 'stop' };
+  }
+
+  return { data: chunk.text, id: stack.id, type: 'text' };
+};
+
+export const AWSBedrockCohereStream = (
+  res: InvokeModelWithResponseStreamResponse | ReadableStream,
+  cb?: ChatStreamCallbacks,
+): ReadableStream<string> => {
+  const streamStack: StreamStack = { id: 'chat_' + nanoid() };
+
+  const stream = res instanceof ReadableStream ? res : createBedrockStream(res);
+
+  return stream
+    .pipeThrough(createSSEProtocolTransformer(transformCohereStream, streamStack))
+    .pipeThrough(createCallbacksTransformer(cb));
+};
