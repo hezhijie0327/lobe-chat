0c547dbc1186ff651df76d6dedc4a38f5f6b6e7f
Zhijie He <hezhijie0327@hotmail.com>
2024-09-30 22:59:26 +0800

    Update route.ts

--- src/app/api/chat/sensenova/route.ts
+++ src/app/api/chat/sensenova/route.ts
@@ -14,14 +14,9 @@ export const POST = async (req: Request) =>
   UniverseRoute(req, {
     createRuntime: (payload) => {
       const { SENSENOVA_ACCESS_KEY_ID, SENSENOVA_ACCESS_KEY_SECRET } = getLLMConfig();
-      let sensenovaAccessKeyID: string | undefined = SENSENOVA_ACCESS_KEY_ID;
-      let sensenovaAccessKeySecret: string | undefined = SENSENOVA_ACCESS_KEY_SECRET;
 
-      // if the payload has the api key, use user
-      if (payload.apiKey) {
-        sensenovaAccessKeyID = payload?.sensenovaAccessKeyID;
-        sensenovaAccessKeySecret = payload?.sensenovaAccessKeySecret;
-      }
+      let sensenovaAccessKeyID: string | undefined = payload?.sensenovaAccessKeyID || SENSENOVA_ACCESS_KEY_ID;
+      let sensenovaAccessKeySecret: string | undefined = payload?.sensenovaAccessKeySecret || SENSENOVA_ACCESS_KEY_SECRET;
 
       const apiKey = generateJwtTokenSenseNova(sensenovaAccessKeyID, sensenovaAccessKeySecret, 60, 15);
 
