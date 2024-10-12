f00c7ff773c81e03201d6ef2e7ff0d60c93392a8
Zhijie He <hezhijie0327@hotmail.com>
2024-10-12 16:48:32 +0800

    Update index.ts

--- src/libs/agent-runtime/sensenova/index.ts
+++ src/libs/agent-runtime/sensenova/index.ts
@@ -37,25 +37,34 @@ const generateJwtTokenSenseNova = (accessKeyID: string = '', accessKeySecret: st
   return apiKey;
 };
 
-export const LobeSenseNovaAI = LobeOpenAICompatibleFactory({
-  baseURL: 'https://api.sensenova.cn/compatible-mode/v1',
-  chatCompletion: {
-    handlePayload: (payload: ChatStreamPayload) => {
-      const { frequency_penalty, temperature, top_p, ...rest } = payload;
+export const LobeSenseNovaAI = (() => {
+  const factory = LobeOpenAICompatibleFactory({
+    baseURL: 'https://api.sensenova.cn/compatible-mode/v1',
+    chatCompletion: {
+      handlePayload: (payload: ChatStreamPayload) => {
+        const { frequency_penalty, temperature, top_p, ...rest } = payload;
+  
+        return {
+          ...rest,
+          frequency_penalty: (frequency_penalty !== undefined && frequency_penalty > 0 && frequency_penalty <= 2) ? frequency_penalty : undefined,
+          temperature: (temperature !== undefined && temperature > 0 && temperature <= 2) ? temperature : undefined,
+          top_p: (top_p !== undefined && top_p > 0 && top_p < 1) ? top_p : undefined,
+        } as OpenAI.ChatCompletionCreateParamsStreaming;
+      }
+    },
+    debug: {
+      chatCompletion: () => process.env.DEBUG_SENSENOVA_CHAT_COMPLETION === '1',
+    },
+    generateJWTToken: (ak: string, sk: string, expiredAfter: number = 1800, notBefore: number = 5) => {
+      return generateJwtTokenSenseNova(ak, sk, expiredAfter, notBefore);
+    },
+    provider: ModelProvider.SenseNova,
+  });
 
-      return {
-        ...rest,
-        frequency_penalty: (frequency_penalty !== undefined && frequency_penalty > 0 && frequency_penalty <= 2) ? frequency_penalty : undefined,
-        temperature: (temperature !== undefined && temperature > 0 && temperature <= 2) ? temperature : undefined,
-        top_p: (top_p !== undefined && top_p > 0 && top_p < 1) ? top_p : undefined,
-      } as OpenAI.ChatCompletionCreateParamsStreaming;
-    }
-  },
-  debug: {
-    chatCompletion: () => process.env.DEBUG_SENSENOVA_CHAT_COMPLETION === '1',
-  },
-  generateJWTToken: (ak: string, sk: string, expiredAfter: number = 1800, notBefore: number = 5) => {
-    return generateJwtTokenSenseNova(ak, sk, expiredAfter, notBefore);
-  },
-  provider: ModelProvider.SenseNova,
-});
+  return {
+    ...factory,
+    generateJWTToken: (ak: string, sk: string, expiredAfter: number = 1800, notBefore: number = 5) => {
+      return generateJwtTokenSenseNova(ak, sk, expiredAfter, notBefore);
+    },
+  };
+})();
