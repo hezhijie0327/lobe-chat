5c1613d56f98ed3f65b0bbeec530d1fc8d74bd57
Zhijie He <hezhijie0327@hotmail.com>
2024-10-13 13:58:40 +0800

    Update route.ts

--- src/app/(backend)/webapi/chat/sensenova/route.ts
+++ src/app/(backend)/webapi/chat/sensenova/route.ts
@@ -14,17 +14,12 @@ export const POST = async (req: Request) => {
   let sensenovaAccessKeyID: string | undefined = SENSENOVA_ACCESS_KEY_ID;
   let sensenovaAccessKeySecret: string | undefined = SENSENOVA_ACCESS_KEY_SECRET;
 
-  console.log(sensenovaAccessKeyID);
-  console.log(sensenovaAccessKeySecret);
-  
   const apiKey = await LobeSenseNovaAI.generateJWTToken(sensenovaAccessKeyID || '', sensenovaAccessKeySecret || '');
-
-  console.log(apiKey);
 
   return UniverseRoute(req, {
     createRuntime: (payload) => {
       const params = {
-        apiKey,  // 传递生成的 apiKey
+        apiKey,
         sensenovaAccessKeyID: payload?.sensenovaAccessKeyID || sensenovaAccessKeyID,
         sensenovaAccessKeySecret: payload?.sensenovaAccessKeySecret || sensenovaAccessKeySecret,
       };
