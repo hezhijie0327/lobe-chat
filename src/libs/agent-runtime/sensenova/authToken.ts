a8704f5b09ca663a4a0e1f6fe34acb056729eacf
Zhijie He <hezhijie0327@hotmail.com>
2024-10-14 14:25:31 +0800

    Update authToken.ts

--- src/libs/agent-runtime/sensenova/authToken.ts
+++ src/libs/agent-runtime/sensenova/authToken.ts
@@ -2,8 +2,6 @@ import { SignJWT } from 'jose';
 
 // https://console.sensecore.cn/help/docs/model-as-a-service/nova/overview/Authorization
 export const generateApiToken = async (apiKey?: string): Promise<string> => {
-  console.log(apiKey);
-  
   if (!apiKey) {
     throw new Error('Invalid apiKey');
   }
@@ -13,8 +11,6 @@ export const generateApiToken = async (apiKey?: string): Promise<string> => {
     throw new Error('Invalid apiKey');
   }
 
-  console.log(id, secret);
-  
   const encoder = new TextEncoder();
 
   const payload = {
@@ -26,8 +22,6 @@ export const generateApiToken = async (apiKey?: string): Promise<string> => {
   const jwt = await new SignJWT(payload)
     .setProtectedHeader({ alg: 'HS256', typ: 'JWT' })
     .sign(encoder.encode(secret));
-
-  console.log(jwt);
 
   return jwt;
 };
