ea8c03053a5a32ab83f25337c9eb0d961bfe1808
Zhijie He <hezhijie0327@hotmail.com>
2024-11-09 07:55:50 +0800

    Update index.ts

--- src/server/globalConfig/index.ts
+++ src/server/globalConfig/index.ts
@@ -36,7 +36,6 @@ export const generateLLMConfig = () => {
       enabled: llmConfig[enabledKey],
       enabledModels: extractEnabledModels(llmConfig[modelListKey]),
       serverModelCards: transformToChatModelCards({
-        // eslint-disable-next-line unicorn/no-useless-fallback-in-spread
         defaultChatModels: providerCard && typeof providerCard === 'object' && 'chatModels' in providerCard
           ? providerCard.chatModels
           : [],
