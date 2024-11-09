30f1024917ef07b2529d1eac37dad52c446b8868
Zhijie He <hezhijie0327@hotmail.com>
2024-11-10 07:57:43 +0800

    Update llm.ts

--- src/const/settings/llm.ts
+++ src/const/settings/llm.ts
@@ -3,8 +3,12 @@ import { ModelProvider } from '@/libs/agent-runtime';
 import { ModelProviderCard } from '@/types/llm';
 import { UserModelProviderConfig } from '@/types/user/settings';
 
-export const DEFAULT_LLM_CONFIG: UserModelProviderConfig = Object.values(ModelProvider).reduce((config, provider) => {
-  const providerCard = ProviderCards[`${provider}ProviderCard` as keyof typeof ProviderCards] as ModelProviderCard;
+export const DEFAULT_LLM_CONFIG: UserModelProviderConfig = Object.keys(ModelProvider).reduce((config, providerKey) => {
+  const provider = ModelProvider[providerKey as keyof typeof ModelProvider]; 
+  console.log(provider)
+  
+  const providerCard = ProviderCards[`${providerKey}ProviderCard` as keyof typeof ProviderCards] as ModelProviderCard;
+  console.log(providerCard)
 
   config[provider] = {
     enabled: provider === ModelProvider.Ollama || provider === ModelProvider.OpenAI,
@@ -12,12 +16,12 @@ export const DEFAULT_LLM_CONFIG: UserModelProviderConfig = Object.values(ModelPr
     ...(provider === ModelProvider.Ollama && { fetchOnClient: true }),
   };
 
-  console.log(config[provider])
+  console.log(config[provider]);
 
   return config;
 }, {} as UserModelProviderConfig);
 
-console.log(DEFAULT_LLM_CONFIG)
+console.log(DEFAULT_LLM_CONFIG);
 
 export const DEFAULT_MODEL = 'gpt-4o-mini';
 export const DEFAULT_EMBEDDING_MODEL = 'text-embedding-3-small';
