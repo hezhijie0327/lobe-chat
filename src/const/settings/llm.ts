bd49857bf424faf6915bd7bfc828f837f98704cd
Zhijie He <hezhijie0327@hotmail.com>
2024-11-10 08:22:05 +0800

    Update llm.ts

--- src/const/settings/llm.ts
+++ src/const/settings/llm.ts
@@ -3,15 +3,16 @@ import { ModelProvider } from '@/libs/agent-runtime';
 import { ModelProviderCard } from '@/types/llm';
 import { UserModelProviderConfig } from '@/types/user/settings';
 
-export const DEFAULT_LLM_CONFIG: UserModelProviderConfig = Object.keys(ModelProvider).reduce((config, providerKey) => {
-  const provider = ModelProvider[providerKey as keyof typeof ModelProvider];
-  const providerCard = ProviderCards[`${providerKey}ProviderCard` as keyof typeof ProviderCards] as ModelProviderCard;
+export const DEFAULT_LLM_CONFIG: UserModelProviderConfig = Object.keys(ModelProvider).reduce((config, provider) => {
+  const providerCard = ProviderCards[`${provider}ProviderCard` as keyof typeof ProviderCards] as ModelProviderCard;
 
   config[provider] = {
-    enabled: provider === ModelProvider.Ollama || provider === ModelProvider.OpenAI,
+    enabled: provider === 'Ollama' || provider === 'OpenAI',
     enabledModels: providerCard ? ProviderCards.filterEnabledModels(providerCard) : [],
-    ...(provider === ModelProvider.Ollama && { fetchOnClient: true }),
+    ...(provider === 'Ollama' && { fetchOnClient: true }),
   };
+
+  console.log(config[provider])
 
   return config;
 }, {} as UserModelProviderConfig);
