05785da2377e028fd0d2dc9309bb23ff1c61ee0c
Zhijie He <hezhijie0327@hotmail.com>
2024-12-16 14:20:32 +0800

    Update spark.ts

--- src/libs/agent-runtime/utils/streams/spark.ts
+++ src/libs/agent-runtime/utils/streams/spark.ts
@@ -35,12 +35,19 @@ export const transformSparkStream = (chunk: OpenAI.ChatCompletionChunk): StreamP
     } as StreamProtocolToolCallChunk;
   }
 
-  if (typeof item.delta?.content === 'string') {
-    return { data: item.delta.content, id: chunk.id, type: 'text' };
+  if (item.finish_reason) {
+    // one-api 的流式接口，会出现既有 finish_reason ，也有 content 的情况
+    //  {"id":"demo","model":"deepl-en","choices":[{"index":0,"delta":{"role":"assistant","content":"Introduce yourself."},"finish_reason":"stop"}]}
+
+    if (typeof item.delta?.content === 'string' && !!item.delta.content) {
+      return { data: item.delta.content, id: chunk.id, type: 'text' };
+    }
+
+    return { data: item.finish_reason, id: chunk.id, type: 'stop' };
   }
 
-  if (item.finish_reason) {
-    return { data: item.finish_reason, id: chunk.id, type: 'stop' };
+  if (typeof item.delta?.content === 'string') {
+    return { data: item.delta.content, id: chunk.id, type: 'text' };
   }
 
   if (item.delta?.content === null) {
