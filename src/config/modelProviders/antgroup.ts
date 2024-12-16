d66dee8e3ac040ced3e770bf0b3c04fccd8ad9f9
Zhijie He <hezhijie0327@hotmail.com>
2024-12-16 22:29:48 +0800

    Update antgroup.ts

--- src/config/modelProviders/antgroup.ts
+++ src/config/modelProviders/antgroup.ts
@@ -5,6 +5,7 @@ const AntGroup: ModelProviderCard = {
     {
       description: '基于万亿级Token语料训练而成，已发布的模型包含Bailing-Lite和Bailing-Pro版本。',
       displayName: 'Bailing Lite 1116',
+      enabled: true,
       id: 'Bailing-Lite-1116',
       pricing: {
         input: 0,
@@ -27,6 +28,7 @@ const AntGroup: ModelProviderCard = {
   checkModel: 'Bailing-Lite-1116',
   description:
     '百灵语言大模型是通用语言大模型，已通过生成式人工智能上线备案',
+  disableBrowserRequest: true,
   id: 'antgroup',
   modelList: { showModelFetcher: true },
   modelsUrl: 'https://www.yuque.com/em8gt4/ymb7rf/gvif12pldt52oa6p',
