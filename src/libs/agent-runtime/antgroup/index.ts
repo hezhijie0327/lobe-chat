4fbe21ca1a0118e5222ab1c10a5c9988489e008f
Zhijie He <hezhijie0327@hotmail.com>
2024-12-16 22:35:30 +0800

    Update index.ts

--- src/libs/agent-runtime/antgroup/index.ts
+++ src/libs/agent-runtime/antgroup/index.ts
@@ -4,7 +4,7 @@ import { LobeOpenAICompatibleFactory } from '../utils/openaiCompatibleFactory';
 export const LobeAntGroupAI = LobeOpenAICompatibleFactory({
   baseURL: 'https://bailingchat.alipay.com',
   debug: {
-    chatCompletion: () => process.env.DEBUG_ANTGROUP_COMPLETION === '1',
+    chatCompletion: () => process.env.DEBUG_ANTGROUP_CHAT_COMPLETION === '1',
   },
   provider: ModelProvider.AntGroup,
 });
