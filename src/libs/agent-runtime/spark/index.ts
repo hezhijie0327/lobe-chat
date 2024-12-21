f36dfb7d4c4379a67607d8bb010c7a08f759a1d1
Zhijie He <hezhijie0327@hotmail.com>
2024-12-21 17:00:49 +0800

    Update index.ts

--- src/libs/agent-runtime/spark/index.ts
+++ src/libs/agent-runtime/spark/index.ts
@@ -1,12 +1,13 @@
 import { ModelProvider } from '../types';
 import { LobeOpenAICompatibleFactory } from '../utils/openaiCompatibleFactory';
 
-import { SparkAIStream } from '../utils/streams';
+import { transformSparkResponseToStream, SparkAIStream } from '../utils/streams';
 
 export const LobeSparkAI = LobeOpenAICompatibleFactory({
   baseURL: 'https://spark-api-open.xf-yun.com/v1',
   chatCompletion: {
     handleStream: SparkAIStream,
+    handleStreamResponse: transformSparkResponseToStream,
     noUserId: true,
   },
   debug: {
