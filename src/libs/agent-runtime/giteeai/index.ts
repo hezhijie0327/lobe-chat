4eb948c89991dad543f7d6c8223835e5ff4d43d4
Zhijie He <hezhijie0327@hotmail.com>
2025-01-03 17:49:08 +0800

    Update index.ts

--- src/libs/agent-runtime/giteeai/index.ts
+++ src/libs/agent-runtime/giteeai/index.ts
@@ -1,8 +1,13 @@
 import { ModelProvider } from '../types';
 import { LobeOpenAICompatibleFactory } from '../utils/openaiCompatibleFactory';
 
+import { GiteeAIStream } from '../utils/streams';
+
 export const LobeGiteeAI = LobeOpenAICompatibleFactory({
   baseURL: 'https://ai.gitee.com/v1',
+  chatCompletion: {
+    handleStream: GiteeAIStream,
+  },
   debug: {
     chatCompletion: () => process.env.DEBUG_GITEE_AI_CHAT_COMPLETION === '1',
   },
