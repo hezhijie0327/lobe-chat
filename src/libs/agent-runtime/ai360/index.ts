39bec2e22d464e210bd0f0b0440b68cc9b048965
Zhijie He <hezhijie0327@hotmail.com>
2025-01-06 20:51:13 +0800

    Update index.ts

--- src/libs/agent-runtime/ai360/index.ts
+++ src/libs/agent-runtime/ai360/index.ts
@@ -1,6 +1,9 @@
 import { ModelProvider } from '../types';
 import { LobeOpenAICompatibleFactory } from '../utils/openaiCompatibleFactory';
 
+import { LOBE_DEFAULT_MODEL_LIST } from '@/config/modelProviders';
+import { Ai360ModelCard } from './type';
+
 export const LobeAi360AI = LobeOpenAICompatibleFactory({
   baseURL: 'https://api.360.cn/v1',
   chatCompletion: {
@@ -14,5 +17,19 @@ export const LobeAi360AI = LobeOpenAICompatibleFactory({
   debug: {
     chatCompletion: () => process.env.DEBUG_AI360_CHAT_COMPLETION === '1',
   },
+  models: {
+    transformModel: (m) => {
+      const model = m as unknown as Ai360ModelCard;
+
+      return {
+        contextWindowTokens: model.total_tokens,
+        enabled: LOBE_DEFAULT_MODEL_LIST.find((m) => model.id.endsWith(m.id))?.enabled || false,
+        id: model.id,
+        maxTokens:
+          typeof model.max_tokens === 'number'
+            ? model.max_tokens
+            : undefined,
+      };
+    },
   provider: ModelProvider.Ai360,
 });
