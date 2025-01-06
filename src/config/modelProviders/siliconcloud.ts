2f33790b8123ad20a5151bb488226caa6b42316f
Zhijie He <hezhijie0327@hotmail.com>
2025-01-06 21:35:48 +0800

    Update siliconcloud.ts

--- src/config/modelProviders/siliconcloud.ts
+++ src/config/modelProviders/siliconcloud.ts
@@ -592,18 +592,13 @@ const SiliconCloud: ModelProviderCard = {
   checkModel: 'Qwen/Qwen2.5-7B-Instruct',
   description: 'SiliconCloud，基于优秀开源基础模型的高性价比 GenAI 云服务',
   id: 'siliconcloud',
-  modelList: { showModelFetcher: true },
   modelsUrl: 'https://siliconflow.cn/zh-cn/models',
   name: 'SiliconCloud',
-  proxyUrl: {
-    placeholder: 'https://api.siliconflow.cn/v1',
-  },
   settings: {
     proxyUrl: {
       placeholder: 'https://api.siliconflow.cn/v1',
     },
     sdkType: 'openai',
-    showModelFetcher: true,
   },
   url: 'https://siliconflow.cn/zh-cn/siliconcloud',
 };
