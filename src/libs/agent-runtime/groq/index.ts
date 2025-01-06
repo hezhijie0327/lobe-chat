da8af08ae9ab6259d7c0f180a98e79f0cf43420d
Zhijie He <hezhijie0327@hotmail.com>
2025-01-06 21:16:30 +0800

    Update index.ts

--- src/libs/agent-runtime/groq/index.ts
+++ src/libs/agent-runtime/groq/index.ts
@@ -2,6 +2,13 @@ import { AgentRuntimeErrorType } from '../error';
 import { ModelProvider } from '../types';
 import { LobeOpenAICompatibleFactory } from '../utils/openaiCompatibleFactory';
 
+import { LOBE_DEFAULT_MODEL_LIST } from '@/config/modelProviders';
+
+export interface GroqModelCard {
+  context_window: number;
+  id: string;
+}
+
 export const LobeGroq = LobeOpenAICompatibleFactory({
   baseURL: 'https://api.groq.com/openai/v1',
   chatCompletion: {
@@ -23,6 +30,19 @@ export const LobeGroq = LobeOpenAICompatibleFactory({
   },
   debug: {
     chatCompletion: () => process.env.DEBUG_GROQ_CHAT_COMPLETION === '1',
+  },
+  models: {
+    transformModel: (m) => {
+      const model = m as unknown as GroqModelCard;
+
+      return {
+        contextWindowTokens: model.context_window,
+        enabled: LOBE_DEFAULT_MODEL_LIST.find((m) => model.id.endsWith(m.id))?.enabled || false,
+        functionCall: model.id.includes('tool'),
+        id: model.id,
+        vision: model.id.includes('vision'),
+      };
+    },
   },
   provider: ModelProvider.Groq,
 });
