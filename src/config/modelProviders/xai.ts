b03efb3bec28d264b8c9344980a9673b68953997
Zhijie He <hezhijie0327@hotmail.com>
2025-01-06 22:57:29 +0800

    Update xai.ts

--- src/config/modelProviders/xai.ts
+++ src/config/modelProviders/xai.ts
@@ -60,8 +60,12 @@ const XAI: ModelProviderCard = {
   description:
     'xAI 是一家致力于构建人工智能以加速人类科学发现的公司。我们的使命是推动我们对宇宙的共同理解。',
   id: 'xai',
+  modelList: { showModelFetcher: true },
   modelsUrl: 'https://docs.x.ai/docs#models',
   name: 'xAI',
+  proxyUrl: {
+    placeholder: 'https://api.x.ai/v1',
+  },
   settings: {
     proxyUrl: {
       placeholder: 'https://api.x.ai/v1',
