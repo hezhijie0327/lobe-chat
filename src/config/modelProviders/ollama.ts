72b1fe9e0f14bcd7bf5bdaa91bda730e7c02f862
Zhijie He <hezhijie0327@hotmail.com>
2025-01-06 22:02:59 +0800

    Update ollama.ts

--- src/config/modelProviders/ollama.ts
+++ src/config/modelProviders/ollama.ts
@@ -326,13 +326,13 @@ const Ollama: ModelProviderCard = {
       vision: true,
     },
   ],
-  defaultShowBrowserRequest: true,
   description:
     'Ollama 提供的模型广泛涵盖代码生成、数学运算、多语种处理和对话互动等领域，支持企业级和本地化部署的多样化需求。',
   id: 'ollama',
   modelsUrl: 'https://ollama.com/library',
   name: 'Ollama',
   settings: {
+    defaultShowBrowserRequest: true,
     disableBrowserRequest: true,
     sdkType: 'ollama',
     showApiKey: false,
