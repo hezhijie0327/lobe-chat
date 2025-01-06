7b58fa9778d0655479691de08ab6c0faf1628187
Zhijie He <hezhijie0327@hotmail.com>
2025-01-06 21:39:09 +0800

    Update taichu.ts

--- src/config/modelProviders/taichu.ts
+++ src/config/modelProviders/taichu.ts
@@ -27,12 +27,10 @@ const Taichu: ModelProviderCard = {
   description:
     '中科院自动化研究所和武汉人工智能研究院推出新一代多模态大模型，支持多轮问答、文本创作、图像生成、3D理解、信号分析等全面问答任务，拥有更强的认知、理解、创作能力，带来全新互动体验。',
   id: 'taichu',
-  modelList: { showModelFetcher: true },
   modelsUrl: 'https://ai-maas.wair.ac.cn/#/doc',
   name: 'Taichu',
   settings: {
     sdkType: 'openai',
-    showModelFetcher: true,
   },
   url: 'https://ai-maas.wair.ac.cn',
 };
