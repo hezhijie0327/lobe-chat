c3372de08efea530e2e2ce6b48452b155257c6f2
Zhijie He <hezhijie0327@hotmail.com>
2025-01-06 22:47:07 +0800

    Update groq.ts

--- src/config/modelProviders/groq.ts
+++ src/config/modelProviders/groq.ts
@@ -173,8 +173,12 @@ const Groq: ModelProviderCard = {
   description:
     'Groq 的 LPU 推理引擎在最新的独立大语言模型（LLM）基准测试中表现卓越，以其惊人的速度和效率重新定义了 AI 解决方案的标准。Groq 是一种即时推理速度的代表，在基于云的部署中展现了良好的性能。',
   id: 'groq',
+  modelList: { showModelFetcher: true },
   modelsUrl: 'https://console.groq.com/docs/models',
   name: 'Groq',
+  proxyUrl: {
+    placeholder: 'https://api.groq.com/openai/v1',
+  },
   settings: {
     proxyUrl: {
       placeholder: 'https://api.groq.com/openai/v1',
