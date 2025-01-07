4ecc0d17902ca7c108048e1befb1079b8e39ef6e
Zhijie He <hezhijie0327@hotmail.com>
2025-01-07 14:34:47 +0800

    Update index.ts

--- src/libs/agent-runtime/minimax/index.ts
+++ src/libs/agent-runtime/minimax/index.ts
@@ -13,7 +13,7 @@ export const LobeMinimaxAI = LobeOpenAICompatibleFactory({
         presence_penalty: undefined,
         stream: true,
         temperature: temperature === undefined || temperature <= 0 ? undefined : temperature / 2,
-        top_p: top_p === 0 ? undefined : top_p,
+        top_p: top_p !== undefined && top_p > 0 && top_p <= 1 ? top_p : undefined,
       } as any;
     },
   },
