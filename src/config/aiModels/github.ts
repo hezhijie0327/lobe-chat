9b11f47dc99c20d22302752bd4d65109c5269933
Zhijie He <hezhijie0327@hotmail.com>
2025-02-02 07:17:18 +0800

    Update github.ts

--- src/config/aiModels/github.ts
+++ src/config/aiModels/github.ts
@@ -2,9 +2,6 @@ import { AIChatModelCard } from '@/types/aiModel';
 
 const githubChatModels: AIChatModelCard[] = [
   {
-    abilities: {
-      vision: true,
-    },
     contextWindowTokens: 200_000,
     description:
       'o3-mini 是我们最新的小型推理模型，在与 o1-mini 相同的成本和延迟目标下提供高智能。',
