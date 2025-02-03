c8ed47d16ba0c9296aee33b107da7bb2d6446c59
Zhijie He <hezhijie0327@hotmail.com>
2025-02-03 19:32:29 +0800

    Update useModelSupportReasoning.ts

--- src/hooks/useModelSupportReasoning.ts
+++ src/hooks/useModelSupportReasoning.ts
@@ -7,7 +7,7 @@ export const useModelSupportReasoning = (model: string, provider: string) => {
   const newValue = useAiInfraStore(aiModelSelectors.isModelSupportReasoning(model, provider));
 
   // TODO: remove this in V2.0
-  const oldValue = useUserStore(modelProviderSelectors.isModelSupportReasoning(model));
+  const oldValue = useUserStore(modelProviderSelectors.isModelEnabledReasoning(model));
   if (isDeprecatedEdition) return oldValue;
   //
 
