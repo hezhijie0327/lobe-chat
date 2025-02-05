9aedb91bb136e7bbffababc9fbaa1ec5ef2d9b93
Zhijie He <hezhijie0327@hotmail.com>
2025-02-05 20:59:34 +0800

    Update index.ts

--- src/libs/agent-runtime/togetherai/index.ts
+++ src/libs/agent-runtime/togetherai/index.ts
@@ -42,12 +42,12 @@ export const LobeTogetherAI = LobeOpenAICompatibleFactory({
           description: model.description,
           displayName: model.display_name,
           enabled: LOBE_DEFAULT_MODEL_LIST.find((m) => model.name.endsWith(m.id))?.enabled || false,
-          functionCall: model.description?.includes('function calling'),
+          functionCall: model.description?.toLowerCase().includes('function calling'),
           id: model.name,
           maxOutput: model.context_length,
-          reasoning: model.description?.includes('reasoning') || reasoningKeywords.some(keyword => model.name.toLowerCase().includes(keyword)),
+          reasoning: (model.description?.toLowerCase().includes('reasoning') || reasoningKeywords.some(keyword => model.name.toLowerCase().includes(keyword))) && !model.name.toLowerCase().includes('llama3'),
           tokens: model.context_length,
-          vision: model.description?.includes('vision') || model.name?.includes('vision'),
+          vision: model.description?.toLowerCase().includes('vision') || model.name?.toLowerCase().includes('vision'),
         };
       });
   },
