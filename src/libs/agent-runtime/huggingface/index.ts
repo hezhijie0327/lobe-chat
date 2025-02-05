22d56b7a79f6326cac0250d9f73679e468307708
Zhijie He <hezhijie0327@hotmail.com>
2025-02-05 19:29:59 +0800

    Update index.ts

--- src/libs/agent-runtime/huggingface/index.ts
+++ src/libs/agent-runtime/huggingface/index.ts
@@ -74,6 +74,8 @@ export const LobeHuggingFaceAI = LobeOpenAICompatibleFactory({
     return modelList
       .map((model) => {
         return {
+          contextWindowTokens: LOBE_DEFAULT_MODEL_LIST.find((m) => model.id.endsWith(m.id))?.contextWindowTokens ?? undefined,
+          displayName: LOBE_DEFAULT_MODEL_LIST.find((m) => model.id.endsWith(m.id))?.displayName ?? undefined,
           enabled: LOBE_DEFAULT_MODEL_LIST.find((m) => model.id.endsWith(m.id))?.enabled || false,
           functionCall: model.tags.some(tag => tag.toLowerCase().includes('function-calling')),
           id: model.id,
