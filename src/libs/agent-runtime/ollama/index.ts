284972ff520c8ac80b2bd4e0263204bc4915633b
Zhijie He <hezhijie0327@hotmail.com>
2025-02-12 19:27:02 +0800

    Update index.ts

--- src/libs/agent-runtime/ollama/index.ts
+++ src/libs/agent-runtime/ollama/index.ts
@@ -20,6 +20,12 @@ import { OllamaStream, convertIterableToStream } from '../utils/streams';
 import { parseDataUri } from '../utils/uriParser';
 import { OllamaMessage } from './type';
 
+import { LOBE_DEFAULT_MODEL_LIST } from '@/config/aiModels';
+
+export interface OllamaModelCard {
+  id: string;
+}
+
 export class LobeOllamaAI implements LobeRuntimeAI {
   private client: Ollama;
 
@@ -102,11 +108,30 @@ export class LobeOllamaAI implements LobeRuntimeAI {
     return await Promise.all(promises);
   }
 
-  async models(): Promise<ChatModelCard[]> {
-    const list = await this.client.list();
-    return list.models.map((model) => ({
-      id: model.name,
-    }));
+  async models() {
+    const modelList: OllamaModelCard[] = await this.client.list();
+
+    return modelList
+      .map((model) => {
+        const knownModel = LOBE_DEFAULT_MODEL_LIST.find((m) => model.name === m.id);
+
+        return {
+          contextWindowTokens: knownModel?.contextWindowTokens ?? undefined,
+          displayName: knownModel?.displayName ?? undefined,
+          enabled: knownModel?.enabled || false,
+          functionCall:
+            knownModel?.abilities?.functionCall
+            || false,
+          id: model.name,
+          reasoning:
+            knownModel?.abilities?.functionCall
+            || false,
+          vision:
+            knownModel?.abilities?.functionCall
+            || false,
+        };
+      })
+      .filter(Boolean) as ChatModelCard[];
   }
 
   private invokeEmbeddingModel = async (payload: EmbeddingsPayload): Promise<Embeddings> => {
