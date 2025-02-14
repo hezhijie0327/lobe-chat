6aa5981cbc54ddb8e96ac874a2b18e24179cf9cc
Zhijie He <hezhijie0327@hotmail.com>
2025-02-14 15:40:53 +0800

    Create jina.ts

---
+++ src/config/modelProviders/jina.ts
@@ -0,0 +1,11 @@
+import { ModelProviderCard } from '@/types/llm';
+
+const Jina: ModelProviderCard = {
+  chatModels: [],
+  id: 'jina',
+  modelList: { showModelFetcher: true },
+  name: 'Jina',
+  url: 'https://jina.ai/deepsearch',
+};
+
+export default Jina; 
