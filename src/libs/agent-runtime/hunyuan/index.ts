7f4dcee070ab6bbc54d87ece4cf5fa83d2e88d67
Zhijie He <hezhijie0327@hotmail.com>
2025-02-24 22:40:53 +0800

    🐛 fix: fix `enable_multimedia` is whitelist function error

--- src/libs/agent-runtime/hunyuan/index.ts
+++ src/libs/agent-runtime/hunyuan/index.ts
@@ -18,7 +18,9 @@ export const LobeHunyuanAI = LobeOpenAICompatibleFactory({
         ...(enabledSearch && {
           citation: true,
           enable_enhancement: true,
+          /*
           enable_multimedia: true,
+          */
           enable_speed_search: process.env.HUNYUAN_ENABLE_SPEED_SEARCH === '1',
           search_info: true
         }),
