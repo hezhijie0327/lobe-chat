45d7dc2c407dfc44e5ccad13ad15df16e1fae68c
Zhijie He <hezhijie0327@hotmail.com>
2025-03-21 00:47:06 +0800

    Update CategoryAvatar.tsx

--- src/tools/web-browsing/components/CategoryAvatar.tsx
+++ src/tools/web-browsing/components/CategoryAvatar.tsx
@@ -9,11 +9,6 @@ interface CategoryAvatarProps {
   size?: number;
 }
 
-interface CategoryAvatarGroupProps {
-  categories: string[];
-  size?: number;
-}
-
 export const CategoryAvatar = memo<CategoryAvatarProps>(
   ({ category, size = 26 }) => {
     const Icon = CATEGORY_ICON_MAP[category];
@@ -34,34 +29,6 @@ export const CategoryAvatar = memo<CategoryAvatarProps>(
           size={size} 
         />
       </Avatar>
-    );
-  },
-);
-
-export const CategoryAvatarGroup = memo<CategoryAvatarGroupProps>(
-  ({ categories, size = 16}) => {
-    const theme = useTheme();
-    
-    return (
-      <Avatar.Group>
-        {categories.map((category) => (
-          <Avatar
-            key={category}
-            style={{
-              background: 'transparent',
-              color: theme.colorTextSecondary,
-              display: 'flex',
-              height: size,
-              width: size,
-            }}
-          >
-            <CategoryAvatar 
-              category={category} 
-              size={size}
-            />
-          </Avatar>
-        ))}
-      </Avatar.Group>
     );
   },
 );
