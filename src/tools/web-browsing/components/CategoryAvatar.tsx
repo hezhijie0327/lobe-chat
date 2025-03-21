ea530dd342d36db14781f8e4d4213abd73cfd609
Zhijie He <hezhijie0327@hotmail.com>
2025-03-21 09:56:08 +0800

    Update CategoryAvatar.tsx

--- src/tools/web-browsing/components/CategoryAvatar.tsx
+++ src/tools/web-browsing/components/CategoryAvatar.tsx
@@ -6,29 +6,21 @@ import { CATEGORY_ICON_MAP } from '../const';
 
 interface CategoryAvatarProps {
   category: string;
-  size?: number;
 }
 
-export const CategoryAvatar = memo<CategoryAvatarProps>(
-  ({ category, size = 26 }) => {
-    const Icon = CATEGORY_ICON_MAP[category];
-    const theme = useTheme();
+export const CategoryAvatar = memo<EngineAvatarProps>(({ category }) => (
+  const theme = useTheme();
 
-    return (
-      <Avatar
-        alt={category}
-        style={{
-          backgroundColor: 'transparent',
-          color: theme.colorTextSecondary,
-          display: 'flex',
-          height: size,
-          width: size,
-        }}
-      >
-        <Icon 
-          size={size} 
-        />
-      </Avatar>
-    );
-  },
-);
+  return (
+    <Avatar
+      alt={category}
+      src={CATEGORY_ICON_MAP[category]}
+      style={{
+        backgroundColor: 'transparent',
+        color: theme.colorTextSecondary,
+        height: 16,
+        width: 16,
+      }}
+    />
+  );
+));
