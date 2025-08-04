4fe117521a2ffcf68d721dbc0a0549da8a36f844
Zhijie He <hezhijie0327@hotmail.com>
2025-08-04 22:55:07 +0800

    Update urlRules.ts

--- packages/web-crawler/src/urlRules.ts
+++ packages/web-crawler/src/urlRules.ts
@@ -100,9 +100,9 @@ export const crawUrlRules: CrawlUrlRule[] = [
     impls: ['jina'],
     urlPattern: 'https://(.*).feishu.cn/(.*)',
   },
-  // 小红书存在爬虫防护，使用 Search1API 或 Jina (备用)
+  // 小红书存在爬虫防护，使用 Search1API 或 Browserless (备用)
   {
-    impls: ['search1api', 'jina'],
+    impls: ['search1api', 'browserless'],
     urlPattern: 'https://(.*).xiaohongshu.com/(.*)',
   },
 ];
